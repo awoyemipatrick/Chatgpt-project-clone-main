483ea7d7ac2f62d19ca09c329b2a743eee745ca5
Georgechisom <128038610+Georgechisom@users.noreply.github.com>
2024-09-11 21:04:03 +0100

    default text added

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 const API_KEY = "sk-Eyee-fmTI0BdAxbxSrDZHQQfcuXd969LJ1rjMhc-ibT3BlbkFJNIDTBUgnNIJ7SqtcSMVMlwCKmLPMjSoF0YRcV3abkA"; // this is a key
 
+const defaultText = `<div class="default-text">
+                            <h1> CHAT GPT PROJECT</h1>
+                            <p>Start a conversation and explore the power of A.I <br> your chat history will display as you chat </p>
+                        </div>`
+
 const createElement = (html, className) => {
     const chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
