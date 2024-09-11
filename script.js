9c5eb7020c3897a327981553c819ca299044f1fb
Georgechisom <128038610+Georgechisom@users.noreply.github.com>
2024-09-11 21:07:12 +0100

    load function
    default text enclosed in a function, sent to local storage. The data is also called from local storage

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 const API_KEY = "sk-Eyee-fmTI0BdAxbxSrDZHQQfcuXd969LJ1rjMhc-ibT3BlbkFJNIDTBUgnNIJ7SqtcSMVMlwCKmLPMjSoF0YRcV3abkA"; // this is a key
 
-const defaultText = `<div class="default-text">
+const loadDataFromLocalstorage = () => {
+    const themecolor = localStorage.getItem("light-theme");
+
+    document.body.classList.toggle("light-mode", themecolor === "light_mode");
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+
+    const defaultText = `<div class="default-text">
                             <h1> CHAT GPT PROJECT</h1>
                             <p>Start a conversation and explore the power of A.I <br> your chat history will display as you chat </p>
                         </div>`
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText ;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+
+}
 
 const createElement = (html, className) => {
     const chatDiv = document.createElement("div");
