76dfa700658cd422e7764158b9225bbbeb99cd9c
Georgechisom <128038610+Georgechisom@users.noreply.github.com>
2024-09-11 21:01:26 +0100

    create new div and apply changes

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
+
+const createElement = (html, className) => {
+    //create new div and apply chat, specified class and set html content of div
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = html;
+    return chatDiv // return the created chat div
+}
